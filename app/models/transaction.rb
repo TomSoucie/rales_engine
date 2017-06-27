d30bac0743c08ace4441042ca26fdff7dbebe44d
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-27 13:44:28 -0600

    removes transaction validation for expiry presence

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,8 @@
 class Transaction < ApplicationRecord
   validates :credit_card_number, presence: true
-  validates :credit_card_expiration_date, presence: true
+  # validates :credit_card_expiration_date, presence: true
   validates :result, presence: true
   validates :invoice_id, presence: true
-  
+
   belongs_to :invoice
 end
