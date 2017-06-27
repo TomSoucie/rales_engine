423b83facb66aa49bd81c636fd9b40d867a8e711
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-27 17:47:13 -0600

    add customers_request_spec

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,7 @@ class Customer < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  has_many :invoices 
+  has_many :invoices
+  has_many :merchants, through: :invoices
+  has_many :items, through: :invoices
 end
