0967f9f8dccdbc10a2769bbd3e024da65bcfa0e5
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-27 10:55:52 -0600

    RSpec setup and migrations

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -4,7 +4,7 @@ describe "Merchants API" do
   it "sends a list of merchants" do
     create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+    get "/api/v1/merchants"
 
     expect(response).to be_success
 
@@ -14,5 +14,34 @@ describe "Merchants API" do
     expect(merchants.count).to eq(3)
     expect(merchant).to have_key("name")
     expect(merchant["name"]).to be_a String
+  end
+
+  it "sends a single merchant" do
+    merchant = create(:merchant)
+    id = merchant.id
+
+    get "/api/v1/merchants/#{merchant.id}"
+
+    expect(response).to be_success
+
+    raw_merchant = JSON.parse(response.body)
+
+    expect(raw_merchant["id"]).to eq(id)
+    expect(raw_merchant).to have_key("name")
+    expect(raw_merchant["name"]).to be_a String
+  end
+end
+
+context "POST /api/v1/merchants" do
+  it "creates a merchant with valid params" do
+    merchant_params = {
+      name: "Onnit"
+    }
+    expect {
+      post "/api/v1/merchants", params: merchant_params
+    }.to change {Merchant.count}.by(1)
+
+    expect(response).to be_success
+    expect(response).to have_http_status(200)
   end
 end
