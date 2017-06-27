8fe7b25564f2468245298502a954595a58d9fc15
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-27 12:57:20 -0600

    Transactions controller updates for transactions_request

--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,21 @@ ActiveRecord::Schema.define(version: 20170627163327) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "invoices", force: :cascade do |t|
-    t.text "status"
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "invoices_items", force: :cascade do |t|
-    t.bigint "item_id"
-    t.bigint "invoice_id"
-    t.integer "quantity"
-    t.decimal "unit_price"
-    t.index ["invoice_id"], name: "index_invoices_items_on_invoice_id"
-    t.index ["item_id"], name: "index_invoices_items_on_item_id"
+  create_table "invoices", force: :cascade do |t|
+    t.bigint "customer_id"
+    t.bigint "merchant_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -46,7 +48,18 @@ ActiveRecord::Schema.define(version: 20170627163327) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "invoices_items", "invoices"
-  add_foreign_key "invoices_items", "items"
+  create_table "transactions", force: :cascade do |t|
+    t.bigint "invoice_id"
+    t.string "credit_card_number"
+    t.string "credit_card_expiration_date"
+    t.string "result"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
+  end
+
+  add_foreign_key "invoices", "customers"
+  add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
+  add_foreign_key "transactions", "invoices"
 end
