8444545839c8fdca8f2df20a7422f864fcf8239b
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-28 13:50:02 -0600

    add relation_request_spec in the merchants/spec folder?

--- spec/requests/api/v1/merchants/find_request_spec.rb
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -61,4 +61,38 @@ describe "Merchant url params returns single merchant" do
     expect(merchant["updated_at"]).to eq(update_time)
   end
 
+  context "GET to merchants/:id/invoices" do
+    it "returns associated collection of invoices" do
+      merchant = create(:merchant)
+      invoices = create_list(:invoice, 2, merchant: merchant.id)
+
+      get "/api/v1/merchants/#{merchant.id}/invoices"
+
+      invoices = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(invoices).to be_instance_of(Array)
+      invoices.each do |invoice|
+        expect(invoice["merchant_id"]).to eq(merchant.id)
+      end
+    end
+  end
+
+  context "GET to merchants/:id/items" do
+    it "returns associated collection of items" do
+      merchant = create(:merchant)
+
+      items = create_list(:item, 2, merchant_id: merchant.id)
+
+      get "/api/v1/merchants/#{merchant.id}/items"
+
+      items = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(items).to be_instance_of(Array)
+      items.each do |i|
+        expect(i["merchant_id"]).to eq(merchant.id)
+      end
+    end
+  end
 end
