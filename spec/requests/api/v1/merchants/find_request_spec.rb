8f155dc01988f1a0f6c7ec93cf9043ad734ece50
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-28 08:22:59 -0600

    add finds a merchant by created_at to merchants find_request_spec

--- spec/requests/api/v1/merchants/find_request_spec.rb
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -17,14 +17,32 @@ describe "Merchant url params returns single merchant" do
   end
 
   xit "finds a merchant by name" do
-    merchants = create(:merchant)
+    merchants = create_list(:merchant, 3)
     name = merchants.first.name
 
     get "/api/v1/merchants/find?name=#{name}"
 
     expect(response).to be_success
-     
 
+    merchant = JSON.parse(response.body)
 
+    expect(merchant).to have_key("name")
+    expect(merchant["name"]).to be_a String
+    expect(merchant["name"]).to eq(name)
+  end
+
+  xit "finds a merchant by created_at" do
+    merchants = create_list(:merchant, 3)
+    create_time = merchants.first.created_at
+
+    get "/api/v1/merchants/find?created_at=#{create_time}"
+
+    expect(response).to be_success
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant).to have_key("created_at")
+    expect(merchant["created_at"]).to be_a String
+    expect(merchant["created_at"]).to eq(create_time)    
   end
 end
