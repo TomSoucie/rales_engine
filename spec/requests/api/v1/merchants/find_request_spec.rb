c863824e8f19e4b287e37d6a255fe91bda7852ab
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-28 13:53:05 -0600

    refactor merchants/find_request and move relational tests to relation_request_spec

--- spec/requests/api/v1/merchants/find_request_spec.rb
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "Merchant url params returns single merchant" do
-  it "find a single merchant from list" do
+describe "Merchants can be found from url params" do
+  it "find a single merchant by id" do
     merchants = create_list(:merchant, 3)
     id = merchants.first.id
 
@@ -59,40 +59,5 @@ describe "Merchant url params returns single merchant" do
     expect(merchant).to have_key("updated_at")
     expect(merchant["updated_at"]).to be_a String
     expect(merchant["updated_at"]).to eq(update_time)
-  end
-
-  context "GET to merchants/:id/invoices" do
-    it "returns associated collection of invoices" do
-      merchant = create(:merchant)
-      invoices = create_list(:invoice, 2, merchant: merchant.id)
-
-      get "/api/v1/merchants/#{merchant.id}/invoices"
-
-      invoices = JSON.parse(response.body)
-
-      expect(response).to be_success
-      expect(invoices).to be_instance_of(Array)
-      invoices.each do |invoice|
-        expect(invoice["merchant_id"]).to eq(merchant.id)
-      end
-    end
-  end
-
-  context "GET to merchants/:id/items" do
-    it "returns associated collection of items" do
-      merchant = create(:merchant)
-
-      items = create_list(:item, 2, merchant_id: merchant.id)
-
-      get "/api/v1/merchants/#{merchant.id}/items"
-
-      items = JSON.parse(response.body)
-
-      expect(response).to be_success
-      expect(items).to be_instance_of(Array)
-      items.each do |i|
-        expect(i["merchant_id"]).to eq(merchant.id)
-      end
-    end
   end
 end
