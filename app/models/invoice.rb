34a4d51ff7137c257d6cf896feabbe96ac02912b
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-28 11:40:23 -0600

    adds code from classwork codealong

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -10,4 +10,23 @@ class Invoice < ApplicationRecord
   has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+
+  def self.most_expensive(limit = 5)
+    select("invoices.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+      .joins(:invoice_items, :transactions).where(transactions: {result: "success"})
+      .group("invoices.id")
+      .order("total_revenue DESC")
+      .limit(limit)
+  end
+
+  # def self.most_expensive(limit = 5)
+  #   select("invoices.*, sum(invoice_items.quantity * invoice_items.unit_price) AS total_revenue")
+  #     .joins(:invoice_items, :transactions)
+  #     # .where(transactions: {result: "success"})
+  #     .merge(Transaction.successful) #need to establish scope in Transaction
+  #     .group("invoices.id")
+  #     .order("total_revenue DESC")
+  #     .limit(limit)
+  # end
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
+
+  # scope :successful -> {where(result: 'success')}
+  # scope :unsuccessful -> {where(result: 'failed')}
+
   validates :credit_card_number, presence: true
   validates :credit_card_expiration_date, presence: false
   validates :result, presence: true
