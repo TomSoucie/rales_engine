7508d19054ce70c8e6c16d7addab9e634602d085
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-27 21:14:04 -0600

    add merchants spec folder and find_request_spec

---
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Merchant url params returns single merchant" do
+  it "find a single merchant from list" do
+    merchants = create_list(:merchant, 3)
+    id = merchants.first.id
+
+    get "/api/v1/merchants/find?id=#{id}"
+
+    expect(response).to be_success
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant).to have_key("id")
+    expect(merchant["id"]).to be_a Integer
+    expect(merchant["id"]).to eq(id)
+  end
+end
