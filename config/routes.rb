a51a6b85bac39b1b4c27a3a93996c02c2cf9e50a
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-29 06:21:46 -0600

    add route and index method to transactions finder_controller

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
       end
 
       namespace :transactions do
+        get 'find_all',      to: 'finder#index'
         get 'find',          to: 'finder#show'
         get ':id/invoice',   to: 'invoice#show'
         get 'random',        to: 'random#show'
