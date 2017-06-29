d1368304426dce611a133faf3fe468e8de369b3d
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-28 19:49:31 -0600

    completes transaction relationship_spec

--- spec/requests/api/v1/transactions/relationship_request_spec.rb
+++ spec/requests/api/v1/transactions/relationship_request_spec.rb
@@ -11,12 +11,12 @@ describe "Transactions have a relationship to an invoice" do
       get "/api/v1/transactions/#{id}/invoice"
       expect(response).to be_success
 
-      invoice_transaction = JSON.parse(response.body)
+      invoice = JSON.parse(response.body)
 
-      expect(invoice_transaction["id"]).to eq(id)
-      expect(invoice_transaction["id"]).to be_a Integer
-      expect(invoice_transaction["invoice_id"]).to eq(invoice_id)
-      expect(invoice_transaction["invoice_id"]).to be_a Integer
+      expect(invoice["id"]).to eq(invoice_id)
+      expect(invoice["id"]).to be_a Integer
+      # expect(invoice_transaction["invoice_id"]).to eq(invoice_id)
+      # expect(invoice_transaction["invoice_id"]).to be_a Integer
 
 
     end
