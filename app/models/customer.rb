c4321e4deaa90247a00a2fa50aa6676e8455a018
Thomas Soucie <tsoucie1858@gmail.com>
2017-06-29 09:10:05 -0600

    updated customer model and outlined customers relationship spec

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,7 @@ class Customer < ApplicationRecord
 
   has_many :invoices
   has_many :merchants, through: :invoices
+  has_many :transactions, through: :invoices
   has_many :items, through: :invoices
 
   def self.random
