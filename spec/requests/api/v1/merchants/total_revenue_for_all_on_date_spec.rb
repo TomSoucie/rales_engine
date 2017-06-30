5b559afe7996588b51c2bfaebd2a5ff55c24873a
rwilhel <rwilhel@gmail.com>
2017-06-29 20:09:46 -0600

    Progress made on business logic

--- spec/requests/api/v1/merchants/total_revenue_for_all_on_date_spec.rb
+++ spec/requests/api/v1/merchants/total_revenue_for_all_on_date_spec.rb
@@ -4,6 +4,10 @@ describe "GET total revenue on given date from all merchants" do
   it "returns the total revenue on x date from all merchants" do
     merchant1 = create(:merchant)
     merchant2 = create(:merchant)
+    merchant3 = create(:merchant)
+
+    today = Date.today
+
     item1 = create(:item, merchant_id: merchant1.id, unit_price: 1000)
     item2 = create(:item, merchant_id: merchant2.id, unit_price: 1000)
   end
